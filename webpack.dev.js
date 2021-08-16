fb87600a0e33164972b6af9af8f58c26ea64e88e
Michael Moore <mmoore4361@gmail.com>
2021-08-16 17:50:19 +0200

    added packages

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 mudule.exports = {
     entry: '.src/client/index.js',
