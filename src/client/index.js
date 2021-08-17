f4c1689fac4f34b7f4a1fdc9081217d4de503a28
Michael Moore <mmoore4361@gmail.com>
2021-08-17 16:41:49 +0200

    fix errors

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import { checkName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
-import { updateUI } from './js/updateUI'
+import { updateUI } from './js/formHandler'
 
 import './styles/header.scss'
 import './styles/main.scss'
