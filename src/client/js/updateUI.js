f2a6064ff9751ed44b9848c6abc31e1c7a9ea308
Michael Moore <mmoore4361@gmail.com>
2021-08-17 12:54:07 +0200

    add code to updateUI function

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,3 +1,13 @@
-function updateUI() {}
+const agreement = document.getElementById('agreement');
+const subjectivity = document.getElementById('subjectivity');
+const confRank = document.getElementById('confidence');
+const irony = document.getElementById('irony');
+
+function updateUI(res) {
+    agreement.innerHTML = `Agreement: ${res.agreement}`;
+    subjectivity.innerHTML = `Subjectivity: ${res.subjectivity}`;
+    confRank.innerHTML = `Confidence Rank: ${res.confidence}%`;
+    irony.innerHTML = `Irony: ${res.irony}`;
+};
 
 export { updateUI }
