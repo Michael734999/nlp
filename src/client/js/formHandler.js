b288fdd22b1112c41015de2595bf182f39bd5e7e
Michael Moore <mmoore4361@gmail.com>
2021-08-17 13:39:11 +0200

    update form handler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,35 @@
-function handleSubmit(e) {}
+import { checkName } from "./nameChecker";
+import { updateUI } from "./updateUI"
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    let url = document.getElementById('name').value;
+
+    if (checkName(url)) {
+        fetch('', {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ url: url })
+            })
+            .then((res) => {
+                res.json()
+            })
+            .then((res) => {
+                console.log(res);
+                updateUI(res);
+            })
+    } else {
+        console.log('Error, invalid URL')
+    }
+};
+
+const search = document.getElementById('submit');
+
+search.addEventListener('click', handleSubmit);
 
 export { handleSubmit }
+export { updateUI }
