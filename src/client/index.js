7c3cf09df716427778496a8fb46495fb26905256
Michael Moore <mmoore4361@gmail.com>
2021-08-17 12:32:39 +0200

    add updateUI to main.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,14 @@
 import { checkName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import { updateUI } from './js/updateUI'
 
 import './styles/header.scss'
 import './styles/main.scss'
 
 export {
     checkName,
-    handleSubmit
+    handleSubmit,
+    updateUI
 }
 
 alert('Hey');
