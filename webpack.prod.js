c618b2dc03a05d8b7d8623b4e4225c0726735280
Michael Moore <mmoore4361@gmail.com>
2021-08-17 18:12:20 +0200

    add cleanwebpackplugin to prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
@@ -34,6 +35,15 @@ module.exports = {
             filename: "./index.html",
         }),
         // new WorkboxPlugin.GenerateSW(),
-        new MiniCssExtractPlugin({ filename: '[name].css' })
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
     ]
 }
