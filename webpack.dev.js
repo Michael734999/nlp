e6749930d2e0733cff1f6ffb2ad3a85f4ae5aba2
Michael Moore <mmoore4361@gmail.com>
2021-08-17 06:44:36 +0200

    fix webpack.dev.js bug

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-mudule.exports = {
+module.exports = {
     entry: '.src/client/index.js',
     output: {
         libraryTarget: 'var',
